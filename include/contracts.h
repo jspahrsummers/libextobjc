c4b72988f823296ea4b0b372921babadeb10ffb8
Justin Spahr-Summers <none@none>
2010-09-01 01:39:05 -0700

    Documentation, of course

--- include/contracts.h
+++ include/contracts.h
@@ -17,6 +17,33 @@
 #include <stdbool.h>
 #include "metamacros.h"
 
+/**
+ * Declares a contract for FUNC. RETTYPE must be the return type of the function,
+ * and any arguments must follow FUNC in the macro invocation.
+ *
+ * Within the contract, body() can be used to execute the implementation of the
+ * function. There must always be an 'in' statement within a contract.
+ *
+ * Example:
+ *
+ * 	contract(void *, allocate_memory, size_t sz) {
+ * 		in {
+ * 			assert(sz <= 1024);
+ * 		}
+ * 		
+ * 		void *ret = body(sz);
+ * 		out {
+ * 			assert(ret != NULL);
+ * 		}
+ * 		
+ * 		return ret;
+ * 	}
+ *
+ * The function definition (which may be in another file) should be implemented
+ * as normal, except with the name changed to FUNC_body() - for example, a
+ * contract on foobar() means that foobar's implementation should actually go
+ * into a function named foobar_body().
+ */
 #define contract(RETTYPE, FUNC, ...) \
 	RETTYPE FUNC ## _body ( __VA_ARGS__ ); \
 	\
@@ -24,7 +51,23 @@
 	RETTYPE FUNC ( __VA_ARGS__ ) {	\
 		RETTYPE (*body)(__VA_ARGS__) = & FUNC ## _body ;
 
-#ifdef NDEBUG
+#ifndef NDEBUG
+	/**
+	 * Defines and documents assumptions about the function's arguments.
+	 *
+	 * One of these statements must always be present (even if empty),
+	 * regardless of whether the function actually has arguments.
+	 */
+	#define in \
+			}
+	
+	/**
+	 * Defines and documents assumptions about the function's return value. This
+	 * statement is optional.
+	 */
+	#define out
+#else
+	// disable execution of contracts if NDEBUG is defined
 	#define in \
 		}	\
 		\
@@ -32,11 +75,6 @@
 	
 	#define out \
 		if (0)
-#else
-	#define in \
-			}
-	
-	#define out
 #endif
 
 #endif
