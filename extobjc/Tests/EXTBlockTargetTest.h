9de7e2bdcac793d7848b7522689c84c4f9d1a9a7
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-04-18 22:14:43 -0700

    Added unit test for EXTBlockTarget (which currently fails)

---
+++ extobjc/Tests/EXTBlockTargetTest.h
@@ -0,0 +1,17 @@
+//
+//  EXTBlockTargetTest.h
+//  extobjc
+//
+//  Created by Justin Spahr-Summers on 2011-04-18.
+//  Released into the public domain.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "extobjc.h"
+
+@interface EXTBlockTargetTest : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ extobjc/Tests/EXTBlockTargetTest.m
@@ -0,0 +1,31 @@
+//
+//  EXTBlockTargetTest.m
+//  extobjc
+//
+//  Created by Justin Spahr-Summers on 2011-04-18.
+//  Released into the public domain.
+//
+
+#import "EXTBlockTargetTest.h"
+
+@implementation EXTBlockTargetTest
+
+- (void)testPerformAfterDelay {
+	__block BOOL executed = NO;
+
+	id target = [EXTBlockTarget
+		blockTargetWithSelector:@selector(setExecuted)
+		action:^{ executed = YES; }
+	];
+
+	STAssertNotNil(target, @"could not initialize EXTBlockTarget instance");
+	STAssertFalse(executed, @"block should not have executed yet");
+
+	[target performSelector:@selector(setExecuted) withObject:nil afterDelay:0];
+	STAssertFalse(executed, @"block should not have executed yet");
+
+	[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+	STAssertTrue(executed, @"block should have executed after run loop has iterated a few times");
+}
+
+@end
