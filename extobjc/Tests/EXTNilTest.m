19e51e92aa9ca1b83b7b025b34731f4cd3073441
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-04-25 02:04:07 -0700

    Expanded EXTNil unit test a bit

--- extobjc/Tests/EXTNilTest.m
+++ extobjc/Tests/EXTNilTest.m
@@ -20,6 +20,13 @@
 	STAssertEquals((NSInteger)[obj length], (NSInteger)0, @"any method on EXTNil object should return zero value");
 	STAssertEqualsWithAccuracy([obj doubleValue], 0.0, 0.01, @"any method on EXTNil object should return zero value");
 	STAssertTrue(NSEqualRanges([obj rangeOfString:@""], NSMakeRange(0, 0)), @"any method on EXTNil object should return zero value");
+	STAssertEqualObjects(obj, obj, @"EXTNil should equal itself");
+	STAssertEqualObjects(obj, [[obj copy] autorelease], @"EXTNil copy should equal EXTNil");
+
+	NSArray *arr = [NSArray arrayWithObject:obj];
+	STAssertNotNil(arr, @"");
+	STAssertEqualObjects([arr objectAtIndex:0], obj, @"EXTNil object properties should be preserved in a collection");
+	STAssertEqualObjects([[arr objectAtIndex:0] target], nil, @"EXTNil object properties should be preserved in a collection");
 }
 
 @end
