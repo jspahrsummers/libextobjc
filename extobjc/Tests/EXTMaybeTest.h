254b32802010df62de0a66d9c8d7d9e4dd80f4a6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-21 16:48:21 -0800

    Unit tests for EXTMaybe

---
+++ extobjc/Tests/EXTMaybeTest.h
@@ -0,0 +1,15 @@
+//
+//  EXTMaybeTest.h
+//  extobjc
+//
+//  Created by Justin Spahr-Summers on 21.01.12.
+//  Released into the public domain.
+//
+
+//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EXTMaybeTest : SenTestCase
+
+@end
