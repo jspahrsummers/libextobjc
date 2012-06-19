c0a30e989cede99e96e0a189d319647a6fae75e3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-06-19 16:12:59 -0700

    Duplicated metamacro_foreach() for recursive invocations

--- extobjc/metamacros.h
+++ extobjc/metamacros.h
@@ -62,6 +62,14 @@
 #define metamacro_foreach(MACRO, ...) \
         metamacro_concat(metamacro_for, metamacro_argcount(__VA_ARGS__))(MACRO, __VA_ARGS__)
 
+/**
+ * Identical to #metamacro_foreach, but can be used when already expanding an
+ * outer invocation to #metamacro_foreach (where another use of it would fail to
+ * expand).
+ */
+#define metamacro_foreach_recursive(MACRO, ...) \
+        metamacro_concat(metamacro_for_recursive, metamacro_argcount(__VA_ARGS__))(MACRO, __VA_ARGS__)
+
 // IMPLEMENTATION DETAILS FOLLOW!
 // Do not write code that depends on anything below this line.
 #define metamacro_stringify_(VALUE) # VALUE
@@ -78,5 +86,16 @@
 #define metamacro_for8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)           metamacro_for7(MACRO, _0, _1, _2, _3, _4, _5, _6)           MACRO(7, _7)
 #define metamacro_for9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)       metamacro_for8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)       MACRO(8, _8)
 #define metamacro_for10(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9)  metamacro_for9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)   MACRO(9, _9)
+
+#define metamacro_for_recursive1(MACRO, _0)                                                                                                             MACRO(0, _0)
+#define metamacro_for_recursive2(MACRO, _0, _1)                                   metamacro_for_recursive1(MACRO, _0)                                   MACRO(1, _1)
+#define metamacro_for_recursive3(MACRO, _0, _1, _2)                               metamacro_for_recursive2(MACRO, _0, _1)                               MACRO(2, _2)
+#define metamacro_for_recursive4(MACRO, _0, _1, _2, _3)                           metamacro_for_recursive3(MACRO, _0, _1, _2)                           MACRO(3, _3)
+#define metamacro_for_recursive5(MACRO, _0, _1, _2, _3, _4)                       metamacro_for_recursive4(MACRO, _0, _1, _2, _3)                       MACRO(4, _4)
+#define metamacro_for_recursive6(MACRO, _0, _1, _2, _3, _4, _5)                   metamacro_for_recursive5(MACRO, _0, _1, _2, _3, _4)                   MACRO(5, _5)
+#define metamacro_for_recursive7(MACRO, _0, _1, _2, _3, _4, _5, _6)               metamacro_for_recursive6(MACRO, _0, _1, _2, _3, _4, _5)               MACRO(6, _6)
+#define metamacro_for_recursive8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)           metamacro_for_recursive7(MACRO, _0, _1, _2, _3, _4, _5, _6)           MACRO(7, _7)
+#define metamacro_for_recursive9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)       metamacro_for_recursive8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)       MACRO(8, _8)
+#define metamacro_for_recursive10(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9)  metamacro_for_recursive9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)   MACRO(9, _9)
 
 #endif
