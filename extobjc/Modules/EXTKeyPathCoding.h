8c5bfae4a31e03a33101d5152dfff7e62706f868
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-06-19 14:47:30 -0700

    Note that @keypath requires at least two components

--- extobjc/Modules/EXTKeyPathCoding.h
+++ extobjc/Modules/EXTKeyPathCoding.h
@@ -10,15 +10,18 @@
 #import "metamacros.h"
 
 /**
- * \@keypath allows compile-time verification of key paths. Given a real key
- * path (e.g., \c str.lowercaseString.UTF8String or \c NSObject.class.version),
- * the macro returns an \c NSString containing all but the first path component
- * (e.g., @"lowercaseString.UTF8String", @"class.version").
+ * \@keypath allows compile-time verification of key paths. Given a real object
+ * receiver and key path (e.g., \c str.lowercaseString.UTF8String or \c
+ * NSObject.class.version), the macro returns an \c NSString containing all but
+ * the first path component (e.g., @"lowercaseString.UTF8String",
+ * @"class.version").
  *
  * In addition to simply creating a key path, this macro ensures that the key
  * path is valid at compile-time (causing a syntax error if not), and supports
  * refactoring, such that changing the name of the property will also update any
  * uses of \@keypath.
+ *
+ * \a PATH must contain at least two components (a receiver and a key).
  */
 #define keypath(PATH) \
     (((void)(NO && ((void)PATH, NO)), strchr(# PATH, '.') + 1))
