bfb1a629cd1df8ce8ef38141e2b9312c4aebe9ca
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-06-19 18:00:13 -0700

    aaaaaaaaaaaaaahhhh i am going insane, but added a bunch of anonymous structs and unions and stuff to get arguments to work transparently

--- extobjc/Modules/EXTADT.h
+++ extobjc/Modules/EXTADT.h
@@ -9,7 +9,9 @@
 #import "metamacros.h"
 
 #define ADT(NAME, ...) \
-    typedef struct metamacro_concat(_ADT_, __LINE__) { \
+    metamacro_foreach(ADT_predef_, __VA_ARGS__) \
+    \
+    typedef struct ADT_CURRENT_T { \
         void *tag; \
         \
         union { \
@@ -19,61 +21,88 @@
     \
     metamacro_foreach(ADT_, __VA_ARGS__)
 
+#define ADT_predef_(INDEX, CONS) \
+    ADT_predef_ ## CONS
+
+#define ADT_predef_constructor(...) \
+    ADT_predef_constructor_(__VA_ARGS__, unsigned char metamacro_concat(metamacro_first(__VA_ARGS__), _unused_))
+
+#define ADT_predef_constructor_(CONS, ...) \
+    metamacro_foreach_cxt(ADT_predef_typedef_, CONS, __VA_ARGS__)
+
+#define ADT_predef_typedef_(INDEX, CONS, PARAM) \
+    typedef metamacro_concat(PARAM, metamacro_concat(_junk_, __LINE__)), ADT_CURRENT_CONS_ALIAS_T(CONS, INDEX);
+
 #define ADT_payload_(INDEX, CONS) \
-    ADT_payload_ ## CONS;
+    ADT_payload_ ## CONS
 
 #define ADT_payload_constructor(...) \
-    ADT_payload_constructor_(__VA_ARGS__, unsigned char)
+    ADT_payload_constructor_(__VA_ARGS__, unsigned char metamacro_concat(metamacro_first(__VA_ARGS__), _unused_))
 
-#define ADT_payload_constructor_(NAME, ...) \
-    struct metamacro_concat(metamacro_concat(_ADT_, __LINE__), NAME) { \
-        metamacro_foreach_recursive(ADT_payload_entry_, __VA_ARGS__) \
-    } NAME
+#define ADT_payload_constructor_(CONS, ...) \
+    struct { \
+        metamacro_foreach_cxt(ADT_payload_entry_, CONS, __VA_ARGS__) \
+    }; \
+    \
+    struct ADT_CURRENT_CONS_ALIASES_T(CONS) { \
+        metamacro_foreach_cxt(ADT_payload_alias_, CONS, __VA_ARGS__) \
+    } CONS;
 
-#define ADT_payload_entry_(INDEX, TYPE) \
-    TYPE v ## INDEX;
+#define ADT_payload_entry_(INDEX, CONS, PARAM) \
+    PARAM;
+
+#define ADT_payload_alias_(INDEX, CONS, PARAM) \
+    ADT_CURRENT_CONS_ALIAS_T(CONS, INDEX) v ## INDEX;
 
 #define ADT_(INDEX, CONS) \
-    ADT_ ## CONS;
+    ADT_ ## CONS
 
 #define ADT_constructor(...) \
-    static inline struct metamacro_concat(_ADT_, __LINE__) \
-    metamacro_foreach_recursive(ADT_prototype_, __VA_ARGS__) \
-    ) { \
-        struct metamacro_concat(_ADT_, __LINE__) s; \
-        \
-        metamacro_foreach_recursive(ADT_initialize_, __VA_ARGS__) \
-        \
+    static inline struct ADT_CURRENT_T \
+    metamacro_first(__VA_ARGS__, 0) (metamacro_foreach_cxt(ADT_prototype_, metamacro_first(__VA_ARGS__, 0), __VA_ARGS__)) \
+    { \
+        metamacro_foreach_cxt(ADT_initialize_, metamacro_first(__VA_ARGS__, 0), __VA_ARGS__) \
         return s; \
     }
 
-#define ADT_prototype_(INDEX, ARG) \
-    metamacro_concat(ADT_prototype, INDEX)(ARG)
+#define ADT_prototype_(INDEX, CONS, PARAM) \
+    metamacro_concat(ADT_prototype, INDEX)(CONS)
 
-#define ADT_prototype0(NAME) NAME (
-#define ADT_prototype1(TYPE) TYPE v0
-#define ADT_prototype2(TYPE) TYPE v1
-#define ADT_prototype3(TYPE) TYPE v2
-#define ADT_prototype4(TYPE) TYPE v3
-#define ADT_prototype5(TYPE) TYPE v4
-#define ADT_prototype6(TYPE) TYPE v5
-#define ADT_prototype7(TYPE) TYPE v6
-#define ADT_prototype8(TYPE) TYPE v7
-#define ADT_prototype9(TYPE) TYPE v8
+#define ADT_prototype0(CONS)
+#define ADT_prototype1(CONS) ADT_CURRENT_CONS_ALIAS_T(CONS, 0) v0
+#define ADT_prototype2(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 1) v1
+#define ADT_prototype3(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 2) v2
+#define ADT_prototype4(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 3) v3
+#define ADT_prototype5(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 4) v4
+#define ADT_prototype6(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 5) v5
+#define ADT_prototype7(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 6) v6
+#define ADT_prototype8(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 7) v7
+#define ADT_prototype9(CONS) , ADT_CURRENT_CONS_ALIAS_T(CONS, 8) v8
 
-#define ADT_initialize_(INDEX, ARG) \
-    metamacro_concat(ADT_initialize, INDEX)(ARG);
+#define ADT_initialize_(INDEX, CONS, PARAM) \
+    metamacro_concat(ADT_initialize, INDEX)(CONS)
 
-#define ADT_initialize0(NAME) \
-        s.tag = (__typeof__(s.tag))&NAME; \
-        struct metamacro_concat(metamacro_concat(_ADT_, __LINE__), NAME) *entry __attribute__((unused)) = &s.NAME
+#define ADT_initialize0(CONS) \
+    struct ADT_CURRENT_T s; \
+    \
+    s.tag = (__typeof__(s.tag))&CONS; \
+    struct ADT_CURRENT_CONS_ALIASES_T(CONS) *entry __attribute__((unused)) = &s.CONS;
 
-#define ADT_initialize1(TYPE) entry->v0 = v0
-#define ADT_initialize2(TYPE) entry->v1 = v1
-#define ADT_initialize3(TYPE) entry->v2 = v2
-#define ADT_initialize4(TYPE) entry->v3 = v3
-#define ADT_initialize5(TYPE) entry->v4 = v4
-#define ADT_initialize6(TYPE) entry->v5 = v5
-#define ADT_initialize7(TYPE) entry->v6 = v6
-#define ADT_initialize8(TYPE) entry->v7 = v7
-#define ADT_initialize9(TYPE) entry->v8 = v8
+#define ADT_initialize1(CONS) entry->v0 = v0;
+#define ADT_initialize2(CONS) entry->v1 = v1;
+#define ADT_initialize3(CONS) entry->v2 = v2;
+#define ADT_initialize4(CONS) entry->v3 = v3;
+#define ADT_initialize5(CONS) entry->v4 = v4;
+#define ADT_initialize6(CONS) entry->v5 = v5;
+#define ADT_initialize7(CONS) entry->v6 = v6;
+#define ADT_initialize8(CONS) entry->v7 = v7;
+#define ADT_initialize9(CONS) entry->v8 = v8;
+
+#define ADT_CURRENT_T \
+    metamacro_concat(_ADT_, __LINE__)
+
+#define ADT_CURRENT_CONS_ALIASES_T(CONS) \
+    metamacro_concat(metamacro_concat(ADT_CURRENT_T, _), metamacro_concat(CONS, _aliases))
+
+#define ADT_CURRENT_CONS_ALIAS_T(CONS, INDEX) \
+    metamacro_concat(ADT_CURRENT_CONS_ALIASES_T(CONS), INDEX)
--- extobjc/Tests/EXTADTTest.m
+++ extobjc/Tests/EXTADTTest.m
@@ -8,22 +8,29 @@
 
 #import "EXTADTTest.h"
 
-ADT(MyType,
+ADT(Color,
     constructor(Red),
     constructor(Green),
     constructor(Blue),
-    constructor(Other, int)
+    constructor(Gray, double alpha),
+    constructor(Other, double r, double g, double b)
 );
 
 @implementation EXTADTTest
 
 - (void)testConstruction {
-    MyType t = Red();
-    STAssertTrue(t.tag == Red, @"");
+    Color c1 = Red();
+    STAssertTrue(c1.tag == Red, @"");
 
-    MyType t2 = Other(123);
-    STAssertTrue(t2.tag == Other, @"");
-    STAssertEquals(t2.Other.v0, 123, @"");
+    Color c2 = Other(1.0, 0.5, 0.25);
+    STAssertTrue(c2.tag == Other, @"");
+    STAssertEqualsWithAccuracy(c2.r, 1.0, 0.0001, @"");
+    STAssertEqualsWithAccuracy(c2.g, 0.5, 0.0001, @"");
+    STAssertEqualsWithAccuracy(c2.b, 0.25, 0.0001, @"");
+
+    Color c3 = Gray(0.75);
+    STAssertTrue(c3.tag == Gray, @"");
+    STAssertEqualsWithAccuracy(c3.alpha, 0.75, 0.0001, @"");
 }
 
 @end
