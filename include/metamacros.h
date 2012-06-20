237d6e76da9cea60348d9a3bb383a485732c2a21
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-06-19 23:39:33 -0700

    Renamed the private macros behind metamacro_foreach() and metamacro_foreach_cxt() to match the public ones

--- include/metamacros.h
+++ include/metamacros.h
@@ -52,14 +52,14 @@
  * Inspired by P99: http://p99.gforge.inria.fr
  */
 #define metamacro_foreach(MACRO, ...) \
-        metamacro_concat(metamacro_for, metamacro_argcount(__VA_ARGS__))(MACRO, __VA_ARGS__)
+        metamacro_concat(metamacro_foreach, metamacro_argcount(__VA_ARGS__))(MACRO, __VA_ARGS__)
 
 /**
  * Identical to #metamacro_foreach, but accepts an additional context argument,
  * which will be passed through to MACRO after the index argument.
  */
 #define metamacro_foreach_cxt(MACRO, CONTEXT, ...) \
-        metamacro_concat(metamacro_for_cxt, metamacro_argcount(__VA_ARGS__))(MACRO, CONTEXT, __VA_ARGS__)
+        metamacro_concat(metamacro_foreach_cxt, metamacro_argcount(__VA_ARGS__))(MACRO, CONTEXT, __VA_ARGS__)
 
 /**
  * Returns the first argument given.
@@ -83,26 +83,26 @@
 #define metamacro_concat_(A, B) A ## B
 #define metamacro_index9_(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, ...) _9
 
-#define metamacro_for1(MACRO, _0)                                                                                                   MACRO(0, _0)
-#define metamacro_for2(MACRO, _0, _1)                                   metamacro_for1(MACRO, _0)                                   MACRO(1, _1)
-#define metamacro_for3(MACRO, _0, _1, _2)                               metamacro_for2(MACRO, _0, _1)                               MACRO(2, _2)
-#define metamacro_for4(MACRO, _0, _1, _2, _3)                           metamacro_for3(MACRO, _0, _1, _2)                           MACRO(3, _3)
-#define metamacro_for5(MACRO, _0, _1, _2, _3, _4)                       metamacro_for4(MACRO, _0, _1, _2, _3)                       MACRO(4, _4)
-#define metamacro_for6(MACRO, _0, _1, _2, _3, _4, _5)                   metamacro_for5(MACRO, _0, _1, _2, _3, _4)                   MACRO(5, _5)
-#define metamacro_for7(MACRO, _0, _1, _2, _3, _4, _5, _6)               metamacro_for6(MACRO, _0, _1, _2, _3, _4, _5)               MACRO(6, _6)
-#define metamacro_for8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)           metamacro_for7(MACRO, _0, _1, _2, _3, _4, _5, _6)           MACRO(7, _7)
-#define metamacro_for9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)       metamacro_for8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)       MACRO(8, _8)
-#define metamacro_for10(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9)  metamacro_for9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)   MACRO(9, _9)
+#define metamacro_foreach1(MACRO, _0)                                                                                                       MACRO(0, _0)
+#define metamacro_foreach2(MACRO, _0, _1)                                   metamacro_foreach1(MACRO, _0)                                   MACRO(1, _1)
+#define metamacro_foreach3(MACRO, _0, _1, _2)                               metamacro_foreach2(MACRO, _0, _1)                               MACRO(2, _2)
+#define metamacro_foreach4(MACRO, _0, _1, _2, _3)                           metamacro_foreach3(MACRO, _0, _1, _2)                           MACRO(3, _3)
+#define metamacro_foreach5(MACRO, _0, _1, _2, _3, _4)                       metamacro_foreach4(MACRO, _0, _1, _2, _3)                       MACRO(4, _4)
+#define metamacro_foreach6(MACRO, _0, _1, _2, _3, _4, _5)                   metamacro_foreach5(MACRO, _0, _1, _2, _3, _4)                   MACRO(5, _5)
+#define metamacro_foreach7(MACRO, _0, _1, _2, _3, _4, _5, _6)               metamacro_foreach6(MACRO, _0, _1, _2, _3, _4, _5)               MACRO(6, _6)
+#define metamacro_foreach8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)           metamacro_foreach7(MACRO, _0, _1, _2, _3, _4, _5, _6)           MACRO(7, _7)
+#define metamacro_foreach9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)       metamacro_foreach8(MACRO, _0, _1, _2, _3, _4, _5, _6, _7)       MACRO(8, _8)
+#define metamacro_foreach10(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9)  metamacro_foreach9(MACRO, _0, _1, _2, _3, _4, _5, _6, _7, _8)   MACRO(9, _9)
 
-#define metamacro_for_cxt1(MACRO, CONTEXT, _0) MACRO(0, CONTEXT, _0)
-#define metamacro_for_cxt2(MACRO, CONTEXT, _0, _1) metamacro_for_cxt1(MACRO, CONTEXT, _0) MACRO(1, CONTEXT, _1)
-#define metamacro_for_cxt3(MACRO, CONTEXT, _0, _1, _2) metamacro_for_cxt2(MACRO, CONTEXT, _0, _1) MACRO(2, CONTEXT, _2)
-#define metamacro_for_cxt4(MACRO, CONTEXT, _0, _1, _2, _3) metamacro_for_cxt3(MACRO, CONTEXT, _0, _1, _2) MACRO(3, CONTEXT, _3)
-#define metamacro_for_cxt5(MACRO, CONTEXT, _0, _1, _2, _3, _4) metamacro_for_cxt4(MACRO, CONTEXT, _0, _1, _2, _3) MACRO(4, CONTEXT, _4)
-#define metamacro_for_cxt6(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5) metamacro_for_cxt5(MACRO, CONTEXT, _0, _1, _2, _3, _4) MACRO(5, CONTEXT, _5)
-#define metamacro_for_cxt7(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6) metamacro_for_cxt6(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5) MACRO(6, CONTEXT, _6)
-#define metamacro_for_cxt8(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7) metamacro_for_cxt7(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6) MACRO(7, CONTEXT, _7)
-#define metamacro_for_cxt9(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7, _8) metamacro_for_cxt8(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7) MACRO(8, CONTEXT, _8)
-#define metamacro_for_cxt10(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9) metamacro_for_cxt9(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7, _8) MACRO(9, CONTEXT, _9)
+#define metamacro_foreach_cxt1(MACRO, CONTEXT, _0) MACRO(0, CONTEXT, _0)
+#define metamacro_foreach_cxt2(MACRO, CONTEXT, _0, _1) metamacro_foreach_cxt1(MACRO, CONTEXT, _0) MACRO(1, CONTEXT, _1)
+#define metamacro_foreach_cxt3(MACRO, CONTEXT, _0, _1, _2) metamacro_foreach_cxt2(MACRO, CONTEXT, _0, _1) MACRO(2, CONTEXT, _2)
+#define metamacro_foreach_cxt4(MACRO, CONTEXT, _0, _1, _2, _3) metamacro_foreach_cxt3(MACRO, CONTEXT, _0, _1, _2) MACRO(3, CONTEXT, _3)
+#define metamacro_foreach_cxt5(MACRO, CONTEXT, _0, _1, _2, _3, _4) metamacro_foreach_cxt4(MACRO, CONTEXT, _0, _1, _2, _3) MACRO(4, CONTEXT, _4)
+#define metamacro_foreach_cxt6(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5) metamacro_foreach_cxt5(MACRO, CONTEXT, _0, _1, _2, _3, _4) MACRO(5, CONTEXT, _5)
+#define metamacro_foreach_cxt7(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6) metamacro_foreach_cxt6(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5) MACRO(6, CONTEXT, _6)
+#define metamacro_foreach_cxt8(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7) metamacro_foreach_cxt7(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6) MACRO(7, CONTEXT, _7)
+#define metamacro_foreach_cxt9(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7, _8) metamacro_foreach_cxt8(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7) MACRO(8, CONTEXT, _8)
+#define metamacro_foreach_cxt10(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9) metamacro_foreach_cxt9(MACRO, CONTEXT, _0, _1, _2, _3, _4, _5, _6, _7, _8) MACRO(9, CONTEXT, _9)
 
 #endif
