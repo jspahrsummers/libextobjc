b01a6cfac14a51a58578ba79c19b73a52efc1950
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-09-04 12:14:09 -0700

    Tests for EXTSynthesize WIP

---
+++ Tests/EXTSynthesizeTest.h
@@ -0,0 +1,14 @@
+//
+//  EXTSynthesizeTest.h
+//  extobjc
+//
+//  Created by Justin Spahr-Summers on 2012-09-04.
+//  Copyright (C) 2012 Justin Spahr-Summers.
+//  Released under the MIT license.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EXTSynthesizeTest : SenTestCase
+
+@end
