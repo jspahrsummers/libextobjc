d2c21d7740f795e4dd4b0758957f32144a180350
Igor Kashkuta <ikashkuta@gmail.com>
2013-04-02 22:10:08 +0700

    Add test to check for objective-c++ compilation
    There are some problems with two headers in clang c++ mode so we need a
    simple test that compiles as objective-c++

---
+++ Tests/EXTObjectiveCppCompileTest.h
@@ -0,0 +1,14 @@
+//
+//  EXTObjectiveCppCompileTest.h
+//  extobjc
+//
+//  Created by Igor Kashkuta on 2013-04-01.
+//  Released under the MIT license.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "extobjc.h"
+
+@interface EXTObjectiveCppCompileTest : SenTestCase
+
+@end
