7e7e6343195594ea461286d2e58a06214f523bcc
Josh Caswell <josh@kraken>
2013-10-28 01:51:22 -0700

    Added @passthrough_property macro, allowing forwarding of a specified property to a property on another class, along with tests and doc string.
    This necessitated some small changes to the existing passthrough_ macro; in particular, a SEL_GETTER must be passed in — this is @selector when the method name is specified literally, but sel_getUid() for properties. A property’s accessor methods must be looked up at runtime. The lookup is implemented by two helper functions, which are in EXTRuntimeExtensions
    
    This commit is intended to resolve #5.

--- Tests/EXTPassthroughTest.m
+++ Tests/EXTPassthroughTest.m
@@ -10,23 +10,36 @@
 #import "EXTPassthroughTest.h"
 
 @interface InnerClass : NSObject
-@property (nonatomic, getter = isEnabled) BOOL enabled;
 
 - (void)voidMethod;
 - (int)methodWithString:(NSString *)str;
 - (int)methodWithString:(NSString *)str number:(NSNumber *)num;
+
+@property (nonatomic, getter = hasFlakyCrust, setter = topWithFlakyCrust:) BOOL flakyCrust;
+@property (nonatomic, getter = isALaMode) BOOL aLaMode;
+@property (strong, nonatomic, setter = assignFilling:, getter = whatIsTheFilling) NSString * filling;
+@property (nonatomic) NSTimeInterval bakingTime;
+
 @end
 
 @interface OuterClass : NSObject
 @property (nonatomic, strong) InnerClass *inner;
+
+@property (nonatomic, getter = hasFlakyCrust, setter = topWithFlakyCrust:) BOOL flakyCrust;
+@property (nonatomic) BOOL aLaMode;
+@property (strong, nonatomic) NSString * fruitType;
+
 @end
 
 @interface OuterClass (DelegatedMethods)
-@property (nonatomic, getter = isEnabled) BOOL enabled;
 
 - (void)renamedMethod;
 - (int)methodWithString:(NSString *)str;
 - (int)methodWithString:(NSString *)str number:(NSNumber *)num;
+@end
+
+@interface OuterClass (DelegatedProperty)
+@property (nonatomic) NSTimeInterval bakingTime;
 @end
 
 @implementation EXTPassthroughTest
@@ -34,22 +47,56 @@
 - (void)testPassthroughMethods {
     OuterClass *outer = [[OuterClass alloc] init];
     STAssertNotNil(outer, @"");
-
+    
     [outer renamedMethod];
     STAssertEquals([outer methodWithString:@"foo"], 3, @"");
     STAssertEquals([outer methodWithString:@"foobar" number:@5], 11, @"");
 }
 
-- (void)testPassthroughProperty {
+- (void)testPassthroughPropertyCustomAccessors {
     OuterClass *outer = [[OuterClass alloc] init];
     STAssertNotNil(outer, @"");
-    STAssertFalse(outer.enabled, @"");
-    STAssertFalse(outer.inner.enabled, @"");
-
-    outer.enabled = YES;
-    STAssertTrue(outer.enabled, @"");
-    STAssertTrue(outer.inner.enabled, @"");
+    STAssertFalse(outer.flakyCrust, @"");
+    STAssertFalse(outer.inner.flakyCrust, @"");
+    
+    outer.flakyCrust = YES;
+    STAssertTrue(outer.flakyCrust, @"");
+    STAssertTrue(outer.inner.flakyCrust, @"");
 }
+
+- (void)testPassthroughPropertySameBaseNameDifferentAccessors {
+    OuterClass *outer = [[OuterClass alloc] init];
+    STAssertNotNil(outer, @"");
+    STAssertFalse(outer.aLaMode, @"");
+    STAssertFalse(outer.inner.aLaMode, @"");
+    
+    outer.aLaMode = YES;
+    STAssertTrue(outer.aLaMode, @"");
+    STAssertTrue(outer.inner.aLaMode, @"");
+}
+
+- (void)testPassthroughPropertyDifferentNames {
+    OuterClass *outer = [[OuterClass alloc] init];
+    STAssertNotNil(outer, @"");
+    STAssertEqualObjects(nil, outer.fruitType, @"");
+    STAssertEqualObjects(nil, outer.inner.filling, @"");
+    
+    outer.fruitType = @"Pear";
+    STAssertEqualObjects(@"Pear", outer.fruitType, @"");
+    STAssertEqualObjects(@"Pear", outer.inner.filling, @"");
+}
+
+- (void)testPassthroughPropertySameNames {
+    OuterClass *outer = [[OuterClass alloc] init];
+    STAssertNotNil(outer, @"");
+    STAssertEquals(0.0, outer.bakingTime, @"");
+    STAssertEquals(0.0, outer.inner.bakingTime, @"");
+    
+    outer.bakingTime = 75.0;
+    STAssertEqualsWithAccuracy(75.0, outer.bakingTime, 0.5, @"");
+    STAssertEqualsWithAccuracy(75.0, outer.inner.bakingTime, 0.5, @"");
+}
+
 
 @end
 
@@ -57,18 +104,24 @@
 @passthrough(OuterClass, renamedMethod, self.inner, voidMethod);
 @passthrough(OuterClass, methodWithString:, self.inner);
 @passthrough(OuterClass, methodWithString:number:, [self inner]);
-@passthrough(OuterClass, isEnabled, self.inner);
-@passthrough(OuterClass, setEnabled:, self.inner);
+
+@passthrough_property(OuterClass, flakyCrust, self.inner);
+@passthrough_property(OuterClass, fruitType, self.inner, filling);
+@passthrough_property(OuterClass, aLaMode, self.inner, aLaMode);
 
 - (id)init {
     self = [super init];
     if (!self)
         return nil;
-
+    
     self.inner = [[InnerClass alloc] init];
     return self;
 }
 
+@end
+
+@implementation OuterClass (DelegatedProperty)
+@passthrough_property(OuterClass, bakingTime, self.inner);
 @end
 
 @implementation InnerClass
