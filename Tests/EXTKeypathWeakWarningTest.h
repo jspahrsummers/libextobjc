df5c43068ee8d43d83d407b925557ad437e010e3
Javier Soto <javier.api@gmail.com>
2014-06-23 11:00:51 -0700

    Added test for silencing Warc-repeated-use-of-weak by promoting it to an error and making sure the file compiles

---
+++ Tests/EXTKeypathWeakWarningTest.h
@@ -0,0 +1,14 @@
+//
+//  EXTKeypathWeakWarningTest.h
+//  extobjc
+//
+//  Created by Javier Soto on 6/23/14.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "EXTKeyPathCoding.h"
+
+@interface EXTKeypathWeakWarningTest : SenTestCase
+
+@end
